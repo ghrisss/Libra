671a1c1c02c286cd6e15938b05116037238b46e4
ghrisss <heriquebati@gmail.com>
2023-12-13 09:17:20 -0300

    chore: apagar pastas de imagens mais antigas

--- src/controllers/files.py
+++ src/controllers/files.py
@@ -1,6 +1,8 @@
 
 from pathlib import Path
 from shutil import move
+import os
+from time import time
 
 from src.configs import DEBUG
 
@@ -18,3 +20,19 @@ class FilesController():
         except Exception as e:
             print("[FrameController] ERRO: arquivo ou diretório não encontrado \n" +
                   f"[FrameController] Tentativa de envio de {root_dir} \n[FrameController] Tentou enviar para {dst_path}")
+            
+    @classmethod
+    def deleteFolder(cls):
+        root_dir = Path(__file__).parent.parent.parent
+        path = f"{root_dir}\\created_files"
+        current_time = time()
+
+        for i, dir in enumerate(os.listdir(path)):
+            dir_path = os.path.join(path, dir)
+            last_mod = os.stat(dir_path).st_mtime
+            if (current_time - last_mod) // (24*3600) >= 7:
+                # for files in os.listdir(dir_path):
+                    # os.remove(files)
+                # os.rmdir(dir_path)
+                if DEBUG:
+                    print(f'{dir} removed')
